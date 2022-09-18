412e00a28d5cf35b40ac744a807e83b3cf335710
Artyom <asatryanartyom.1989@gmail.com>
2022-09-18 21:26:26 +0300

    first commit

---
+++ DZ_02/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+//Например, задан массив:
+
+//1 4 7 2
+
+//5 9 2 3
+
+//8 4 2 4
+
+//5 2 6 7
+
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int colCount = 6;
+int rowCount = 5;
+int[,] arr = GenerateArray(rowCount, colCount);
+PrintArray(arr);
+Console.WriteLine("Сумма элементов строки");
+int [] sumArr = new int [rowCount];
+for (int i=0;i<rowCount;i++)
+{
+    for(int j=0;j<colCount;j++)
+    {
+        sumArr[i]+=arr[i,j];
+    }        
+} 
+Console.WriteLine(string.Join("\t", sumArr));   
+int minValue = sumArr[0];
+int minValuei = 1;
+for (int i = 0; i < rowCount; i++)
+{
+    if(sumArr[i] < minValue)
+   {
+    minValue = sumArr[i];
+    minValuei = i+1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Минимальное значение - " + minValue + "  Находится в строке - " + minValuei);
+Console.WriteLine();
+
+
---
+++ DZ_03/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+
+int colCount = 2;
+int rowCount = 2;
+int[,] arrA = GenerateArray(rowCount, colCount);
+PrintArray(arrA);
+Console.WriteLine();
+int[,] arrB = GenerateArray(rowCount, colCount);
+PrintArray(arrB);
+Console.WriteLine();
+int[,] arrC = new int [rowCount,colCount];
+for (int i = 0; i < arrC.GetLength(0); i++)
+{
+    for (int j = 0; j < arrC.GetLength(1); j++)
+    {
+        for (int k = 0; k < colCount; k++)
+        {
+            arrC[i,j] += arrA[i,k] * arrB[k,j];
+        }
+    Console.Write(arrC[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
